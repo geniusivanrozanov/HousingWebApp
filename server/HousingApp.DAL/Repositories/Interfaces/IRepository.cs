34216c7042a0e6d4d48e45edfb1c5edf5eea6066
Rozanov Ivan <navi.zor9@gmail.com>
2022-12-04 01:18:51 +0300

    add IRepository interface

---
+++ server/HousingApp.DAL/Repositories/Interfaces/IRepository.cs
@@ -0,0 +1,15 @@
+﻿using HousingApp.DAL.Entities.Interfaces;
+
+namespace HousingApp.DAL.Repositories.Interfaces
+{
+    public interface IRepository<TEntity> where TEntity : IEntity
+    {
+        Task<TEntity> Get(int id);
+        Task<IEnumerable<TEntity>> Get();
+        Task<IEnumerable<TEntity>> Get(Func<TEntity, bool> predicate);
+        Task<TEntity> Insert(TEntity entity);
+        Task<IEnumerable<TEntity>> Insert(IEnumerable<TEntity> entities);
+        Task<TEntity> Update(TEntity entity);
+        Task<TEntity> Delete(int id);
+    }
+}
